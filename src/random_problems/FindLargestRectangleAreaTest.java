0758d0d5b80e958682660c3531395fbd70622f40
Vaibhav Tupe <tvaibhav15@gmail.com>
2015-11-11 15:40:08 -0800

    Largest area using Stack with O(n) complexity

--- src/random_problems/FindLargestRectangleAreaTest.java
+++ src/random_problems/FindLargestRectangleAreaTest.java
@@ -34,5 +34,34 @@ public class FindLargestRectangleAreaTest {
 		
 	}
 	
+	@Test
+	public void test4() {
+		FindLargestRectangleArea obj= new FindLargestRectangleArea();
+		int [] array = {1,2,3,4,5};
+		int area = obj.largestArea2(array);
+		assertEquals(9, area);
+		System.out.println("LArgest area = "+ area);
+	}
+	
+	@Test
+	public void test5() {
+		FindLargestRectangleArea obj= new FindLargestRectangleArea();
+		int [] array = {1,2,3,4,5,6};
+		int area = obj.largestArea2(array);
+		assertEquals(12, area);
+		System.out.println("LArgest area = "+ area);
+	}
+	
+	@Test
+	public void test6() {
+		FindLargestRectangleArea obj= new FindLargestRectangleArea();
+		int [] array = {3,5,10,1,2,7};
+		int area = obj.largestArea2(array);
+		System.out.println("LArgest area = "+ area);
+		assertEquals(10, area);
+		
+	}
+	
+	
 
 }
