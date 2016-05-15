2ffe596515f3ad6844ee66cea9c45e2b06cf6aea
Shrinath <Shrinath@Shrinath-PC>
2016-05-15 00:04:02 -0700

    added method to find merge point of two Linked List

--- src/linkedlist/DriveClassLinkedList.java
+++ src/linkedlist/DriveClassLinkedList.java
@@ -4,10 +4,15 @@ public class DriveClassLinkedList {
 
 	public static void main(String[] args){
 		int [] arr1 = {3,2,4,1,5,6,8,9,7};
+		int [] arr2 = {10,14,12,};
 		LinkedListProblems list1 = new LinkedListProblems();
-		
+		LLFindMergePoint mergeList1 = new LLFindMergePoint();
+		LLFindMergePoint mergeList2 = new LLFindMergePoint();
 		//Create linked list
 		list1.createLinkList(arr1);
+		mergeList1.createLinkList(arr1);
+		mergeList2.createLinkList(arr2);
+		
 		//checking if loop present or not if not then display Linked List
 		if(!list1.isLoop()) 
 			list1.displayLinkList();
@@ -15,7 +20,6 @@ public class DriveClassLinkedList {
 		// Find the start point of loop... if loop is not present then print null
 		System.out.print("Start point of loop - ");
 		System.out.println(list1.findLoopPoint(list1.headNode));
-		
 		
 		//inserting loop in Linked list
 		list1.insertLoopInLL(list1.headNode,5);
@@ -29,7 +33,15 @@ public class DriveClassLinkedList {
 		System.out.print("Node count loop - ");
 		System.out.println(list1.loopNodeCount(list1.headNode));
 		
+		//Create merge point
+		mergeList1.createMergePoint(mergeList1.headNode, mergeList2.headNode, 5);
+		mergeList1.displayLinkList();
+		mergeList2.displayLinkList();
 		
+		System.out.print("Intersection point - ");
+		System.out.println(mergeList1.mergePointLL(mergeList1.headNode, mergeList2.headNode).value);
+		
+		mergeList2.printCommonIntersectionOfLL(mergeList1.headNode,mergeList2.headNode);
 	}
 	
 }
--- src/linkedlist/LLFindMergePoint.java
+++ src/linkedlist/LLFindMergePoint.java
@@ -20,6 +20,7 @@ public class LLFindMergePoint extends LinkList{
 		int i=1;
 		while(i<position && trav1.nextNode != null ){
 			trav1 = trav1.nextNode;
+			i++;
 		}
 		
 		while(trav2.nextNode != null){
@@ -30,7 +31,73 @@ public class LLFindMergePoint extends LinkList{
 	}
 	
 	
+	/***
+	 *  This find intersection point of LL
+	 * @param head1 LL1 head
+	 * @param head2 LL2 head
+	 * @return returns intersection node if present or null if not
+	 */
+	public LinkListNode mergePointLL(LinkListNode head1,LinkListNode head2){
+		if(head1 == null || head2 == null){
+			System.out.println("Linked list is not formed yet");
+			return null;
+		}
+		LinkListNode trav1 = head1;
+		LinkListNode trav2 = head2;
+		
+		int count1 = 1;
+		int count2 = 1;
+		
+		while(trav1.nextNode !=null){
+			count1++;
+			trav1 = trav1.nextNode;
+		}
+		
+		while(trav2.nextNode !=null){
+			count2++;
+			trav2 = trav2.nextNode;
+		}
+		
+		trav1 = head1;
+		trav2 = head2;
+		
+		if(count1>count2){
+			for(int i=0;i<count1-count2;i++)
+				trav1 = trav1.nextNode;
+		}
+		else{
+			for(int i=0;i<count2-count1;i++)
+				trav2 = trav2.nextNode;
+		}
+		
+		while(trav1.nextNode !=null){
+			if(trav1.equals(trav2))
+				return trav1;
+			
+			trav1 = trav1.nextNode;
+			trav2 = trav2.nextNode;
+		}
+		
+		return null;
+	}
 	
+	
+	/***
+	 *  This method print the intersected part of two linked list if present
+	 * @param head1 LL1 head
+	 * @param head2 LL2 head 
+	 */
+	public void printCommonIntersectionOfLL(LinkListNode head1, LinkListNode head2){
+		if(head1 == null || head2 == null){
+			System.out.println("Linked list is not formed yet");
+			return;
+		}
+		
+		LinkListNode iHead = mergePointLL(head1,head2);
+		System.out.print("Intersection of two Linked List - ");
+		this.displayLinkList(iHead);
+		
+	}
 	
 	
 }
