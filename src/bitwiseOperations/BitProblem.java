18bbaaadf48693ffd303ca024a698a86a7b9f280
Shrinath <Shrinath@Shrinath-PC>
2016-06-02 16:46:33 -0700

    added java doc

--- src/bitwiseOperations/BitProblem.java
+++ src/bitwiseOperations/BitProblem.java
@@ -2,6 +2,11 @@ package bitwiseOperations;
 
 public class BitProblem {
 	
+	/***
+	 * ***** Find the number of set bits in an interger ******
+	 * @param number find the number of one in this integer
+	 * @return count of number of one
+	 */
 	public int countSetBit(int number){
 		int count = 0;
 		
